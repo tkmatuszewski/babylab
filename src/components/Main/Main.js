98a448cd6a299b2f139cdaabb46d9d6ab2e325d8
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-09-04 00:28:51 +0200

    dependency update

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppMain= ({ children }) => {
+
+    return (
+      <section className="appMain">
+          {children}
+      </section>
+    );
+};
+
+export default AppMain;
