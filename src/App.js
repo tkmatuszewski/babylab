7427a4154404c416db6f8a0f8a624842312c6aae
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-09-05 00:25:58 +0200

    Font import and main partial update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './settings/main.scss';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import * as routes from './constants/routes';
 import Main from './components/Main/Main';
+import SignIn from './components/SignIn/SignIn';
 
 const App =()=> {
   return (
     <Router>
       <Switch>
-        {/* <Route exact path={routes.signIn} component={SignIn} /> */}
-          {/* <Route exact path={routes.signUp} component={SignUp} /> */}
-          <Route exact path={routes.home} component={Main} />
+        <Route exact path={routes.signIn} component={SignIn} />
+        <Route exact path={routes.signUp} component={SignUp} />
+        <Route exact path={routes.home} component={Main} />
           {/* <Route exact path={routes.projects} component={Projects} /> */}
           {/* <Route exact path={routes.add} component={Add} /> */}
       </Switch>
